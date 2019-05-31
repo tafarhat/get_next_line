f12010ca9a03c9cf1bf1578f75ebdacb4d3656da
Taoufiq Farhat <farhat.taoufiq@gmail.com>
2019-05-31 07:30:06 +0100

    realtion les statics functions getline && readline

--- gnl.c
+++ gnl.c
@@ -2,7 +2,9 @@
 
 enum e_read_return_value
 {
-	true = 1, false = 0, err = -1
+	true = 1,
+	false = 0,
+	err = -1
 };
 
 static int gderr(const int fd, char **line)
@@ -14,21 +16,54 @@ static int gderr(const int fd, char **line)
 // 	get_line
 //
 // DESCRIPTION :
-// 	
+// 	L'objectif est de chercher le '\n' dans la ligne,si oui ,on va appeler
+// 	la fonction ft_strchr et remplacer le '\n' par '\0' et on va  stocker
+// 	tous ca dans une variable temporaire, apres on va appeler la fonction 
+// 	ft_strdup pour dipliquer le reste dans la chaine reste
+// 	et retourner true,
+//
 // PARAMS :
-// 	reste, une  
-// 	line, 
+// 	reste, une chaine de caractere ou va stocker le reste 
+// 	line, un tableau du chaine de caractere 
 //
 // RETURN VALUE :
 // 	
-static int ft_getline(char *reste, char **line)  
+static int ft_getline(char **reste, char **line) // faker 3lash khasna char **reste machi char *reste  
 {
+	char *temp;
+	char **old;
+			// *reste = "abcd'\n'efg'\n'hij'\n"
+	if (*reste == NULL)
+		return false;
+	else
+	{
+		old = *reste;
+		temp = ft_strchr(*reste, '\n');       // ===> temp = "abcd"
+		*temp = '\0';			      // ===> temp = "abcd'\0'"
+		*line = ft_strdup(*reste);	      // ===> reste = "efg'\n'hij'\n'"
+		*reste = ft_strdup(temp + 1);
+		ft_strdel(old);
+		return (true);}
 
 }
 
 static int readline(char *reste)
 {
-
+	int ret;
+	char buff[BUFF_SIZE];                      // BUFF_SIZE = 7
+	char *tmp;
+	if (reste == NULL)
+		reste = ft_strnew(0);
+	while (ret = read(fd,buff,BUFF_SIZE) > 0)  //  
+	{
+		buff[ret] = '\0';
+		tmp = *reste;
+		ft_strjoin(*reste,buff);
+		if (ft_strchr(*reste, '\n'))
+			break;
+		ft_strdel(&tmp);
+	}
+	return ret;
 }
 
 int gnl(const int fd, char **line)
@@ -37,7 +72,7 @@ int gnl(const int fd, char **line)
 
 	if (gderr(fd,line))              // gestion d'erreur 
 		return (err);
-	if (ft_getline(reste[fd],line))   // Si la ligne contient un '\n' extracter la ligne et retourner 1
+	if (ft_getline(&reste[fd],line))   // Si la ligne contient un '\n' extracter la ligne et retourner 1
 		return (false);                 
 	int ret = readline(reste[fd]);    // Sinon on va lire jusqu a le '\n'. la valeur de ret signifie ce qui a lire la fonction read  
 
@@ -60,7 +95,7 @@ int gnl(const int fd, char **line)
 	/* 1- verifier les variable par gderr */
 	/* 2- chercher le \n dans le reste */
 	/* 2-1- si oui , la fonction getline ketjbad ligne dans
-		 * le reste et la supprimer	 */
+	 * le reste et la supprimer	 */
 	/* 2-2- Sinon il va reader une partie dans le fd */
 	/* 3-1-	 */
 }
